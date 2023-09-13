895536881ba4e4fce895d888cdc7ac7da8849593
Konstantin Schiller <ks@schillerwebsolutions.com>
2023-09-12 21:48:39 -0300

    Added card and feedbackItem component. Also added delete functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import { useState } from "react";
+import FeedbackData from "./data/FeedbackData";
 
 function App(){
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you ant to delete?')){
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    } // We push this to feedback list then to feedback item and input there the id but wee need the function to be in app.js because there is the data. This will be easier with context api
+
+    const [feedback, setFeedback] = useState(FeedbackData)
+
     return ( //JSX needs 1 element to be returned so we need to wrap in div or an fragment <> if multiple elements
         <>
         <Header />
         <div className='container'>
-             <h1>My App</h1>
+             <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
         </div>
         </>
     )
