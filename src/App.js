1322a0463f582948bc303fd219f7552146537337
Konstantin Schiller <ks@schillerwebsolutions.com>
2023-09-14 21:38:55 -0300

    added react router and also finished the feedback submit form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Header from "./components/Header";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid';
 import FeedbackList from "./components/FeedbackList";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
 
 function App(){
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you ant to delete?')){
             setFeedback(feedback.filter((item) => item.id !== id))
@@ -15,14 +24,22 @@ function App(){
     const [feedback, setFeedback] = useState(FeedbackData)
 
     return ( //JSX needs 1 element to be returned so we need to wrap in div or an fragment <> if multiple elements
-        <>
+        <Router>
         <Header />
         <div className='container'>
-            <FeedbackForm />
-            <FeedbackStats feedback={feedback} />
-             <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+            <Routes>
+            <Route exact path='/' element={
+                <>
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                </>
+            }>
+             </Route>
+             <Route path='/about' element={<AboutPage/>} />
+             </Routes>
         </div>
-        </>
+        </Router>
     )
 };
 
