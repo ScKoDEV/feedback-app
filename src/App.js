7c2c988fb88d6a6e2d5b79847bfbb8c170ea25ae
Konstantin Schiller <ks@schillerwebsolutions.com>
2023-09-16 08:35:20 -0300

    Added about page and routing for this. Also added context and cleaned up all this mess in app js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,35 @@
 import Header from "./components/Header";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import { v4 as uuidv4 } from 'uuid';
 import FeedbackList from "./components/FeedbackList";
-import { useState } from "react";
-import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
+import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App(){
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
-    }
-
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you ant to delete?')){
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
-    } // We push this to feedback list then to feedback item and input there the id but wee need the function to be in app.js because there is the data. This will be easier with context api
-
-    const [feedback, setFeedback] = useState(FeedbackData)
-
     return ( //JSX needs 1 element to be returned so we need to wrap in div or an fragment <> if multiple elements
+    <FeedbackProvider>
         <Router>
         <Header />
         <div className='container'>
             <Routes>
             <Route exact path='/' element={
                 <>
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                <FeedbackForm />
+                <FeedbackStats />
+                <FeedbackList />
                 </>
             }>
              </Route>
              <Route path='/about' element={<AboutPage/>} />
              </Routes>
+             <AboutIconLink />
         </div>
         </Router>
+        </FeedbackProvider>
     )
 };
-
-
 
 export default App;
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,12 +3,17 @@ import Card from './shared/Card'
 import { useState } from 'react'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackForm({handleAdd}) {
+function FeedbackForm() {
+    
     const [text, setText] = useState('')
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
     const [rating, setRating] = useState(10)
+
+    const {addFeedback} = useContext(FeedbackContext)
 
     const handleTextChange = (e) => {
         if(text === ''){
@@ -32,7 +37,7 @@ const handleSubmit = (e) => {
             rating
         }
 
-        handleAdd(newFeedback)
+        addFeedback(newFeedback)
 
         setText('')
     }
